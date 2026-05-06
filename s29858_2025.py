1596b91ba05bace91e4cc3fe97c43022bd1106f6
Kate <levkovickata4@gmail.com>
2026-05-06 21:59:36 +0200

    write Main fun

--- s29858_2025.py
+++ s29858_2025.py
@@ -21,14 +21,14 @@ def calculate_stats(sequence: str) -> dict:
 def insert_name(sequence: str, name: str) -> str:
     """Wstawia imię w losową pozycję sekwencji.
     Imię zapisane małymi literami."""
-    position = random.randit(0, len(sequence))
+    position = random.randint(0, len(sequence))
     name_lower = name.lower()
     return sequence[:position] + name_lower + sequence[position:]
 
 def format_fasta(seq_id: str, description: str,
     sequence: str, line_width: int = 80) -> str:
     """Zwraca sformatowany rekord FASTA jako string."""
-    header = f">{seq_id}{description}".strip()
+    header = f">{seq_id} {description}".strip()
     lines = [sequence[i:i+line_width] for i in range(0, len(sequence), line_width)]
     return header + "\n" + "\n".join(lines) + "\n# EOF_1"
 
@@ -59,4 +59,24 @@ def validate_id(seq_id: str) ->str:
 
 def main():
     """Wiadomo."""
+    length = validate_positive_int("Podaj długość sekwencji: ")
+    seq_id = input("Podaj ID sekwencji: ")
+    seq_id = validate_id(seq_id)
+    description = input("Podaj opis sekwencji: ")
+    name = input("Podaj imię: ")
+    sequence = generate_sequence(length)
+    stats = calculate_stats(sequence)
+    sequence_with_name = insert_name(sequence, name)
+    fasta_text = format_fasta(seq_id, description, sequence_with_name)
+    with open(f"{seq_id}.fasta", "w") as f:
+        f.write(fasta_text)
+    print(f"\nSekwencja zapisana do pliku: {seq_id}.fasta\n")
+
+    print(f"Statystyki sekwencji (n={length}):")
+    for base in "ACGT":
+        print(f"{base}: {stats[base]:.2f}%")
+
+    print(f"GC-content: {stats['gc_ratio_A']:.2f}%")
+
+    
 
